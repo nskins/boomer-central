965d9e716d49561df4d5d8019a2c4418ae81ba2f
Nicholas Skinsacos <nskins@umich.edu>
2017-10-22 19:49:49 -0400

    Serve an empty HTML page

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
 const http = require('http');
 
 const port = process.argv[2];
 
 var server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end();
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end();
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port);
