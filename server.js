1163dfe3d002319483ed428364f781c77162fddb
Nicholas Skinsacos <nskins@umich.edu>
2017-10-23 19:01:10 -0400

    Install pug & rewrite w/ pug

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+
+// Initialize application.
 const app = express();
 
+// Initialize view engine.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.listen(3000);
