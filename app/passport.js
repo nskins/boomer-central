30c08182ce396708a6fc2fda488a03a8cb01c5b1
Nicholas Skinsacos <nskins@umich.edu>
2017-10-28 22:14:52 -0400

    Reorganize directories & isolate passport code

---
+++ app/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user.js');
+
+module.exports = () => {
+  passport.serializeUser(function (user, done) {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    User.findOne({ 'username': username }, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (user.password != password) { return done(null, false); }
+      return done(null, user);
+    });
+  }));
+};
--- server.js
+++ server.js
@@ -3,21 +3,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const session = require('express-session');
-const User = require('./models/user.js');
 
 // Connect to MongoDB.
-const dbConfig = require('./db.js');
+const dbConfig = require('./app/db');
 mongoose.connect(dbConfig.url, {useMongoClient: true});
 
 // Initialize application.
 const app = express();
 
 // Initialize view engine.
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'pug');
 
 // Static resource files.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 // Session handling.
 app.use(session({
@@ -32,28 +31,9 @@ app.use(bodyParser.json());
 
 // Authentication.
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 app.use(passport.initialize());
 app.use(passport.session());
-
-passport.serializeUser(function (user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy((username, password, done) => {
-  User.findOne({ 'username': username }, (err, user) => {
-    if (err) { return done(err); }
-    if (!user) { return done(null, false); }
-    if (user.password != password) { return done(null, false); }
-    return done(null, user);
-  });
-}));
+require('./app/passport')();
 
 app.get('/', function (req, res) {
   res.render('index', { user: req.user });
