8167c22f15ddce95fc17b2756212af1762e11e87
Nicholas Skinsacos <nskins@umich.edu>
2017-11-13 20:04:36 -0500

    Default user avatar: display on all & show

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,6 @@ module.exports = mongoose.model('User', {
   username: String,
   password: String,
   admin: { type: Boolean, default: false },
+  avatar: { type: String, default: '/img/default_avatar.png' },
   favoriteTrick: String
 });
