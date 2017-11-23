94ab6948b88926eadfadd64e5e8cea330ce6717a
Nicholas Skinsacos <nskins@umich.edu>
2017-11-23 12:25:09 -0500

    Test load of layout's other CSS/JS resource files

--- test/layout.test.js
+++ test/layout.test.js
@@ -1,17 +1,25 @@
 const helper = require('./helper');
 const Nightmare = require('nightmare');
+const path = require('path');
 
 const url = 'http://localhost:3000/';
 
 // Test that the specified resource (name) loads.
 // It can be found by the selector on the layout page.
 var testResourceLoads = (name, selector) => {
-  test(name + ' loads without error', done => {
+  var extension = path.extname(name);
+  test(name + ' should load without error', done => {
     new Nightmare()
       .goto(url)
-      .evaluate(selector => {
-        return document.querySelector(selector).href;
-      }, selector)
+      .evaluate((extension, selector) => {
+        var node = document.querySelector(selector);
+        switch(extension) {
+          case '.css':
+            return node.href;
+          case '.js':
+            return node.src;
+        }
+      }, extension, selector)
       .end()
       .then(href => {
         helper.pageLoads(href, done);
@@ -21,5 +29,9 @@ var testResourceLoads = (name, selector) => {
 };
 
 describe('Layout', () => {
-  testResourceLoads('Bootstrap CSS', '#bootstrap-css');
+  testResourceLoads('bootstrap.css', '#bootstrap-css');
+  testResourceLoads('style.css', '#style-css');
+  testResourceLoads('jquery.js', '#jquery-js');
+  testResourceLoads('popper.js', '#popper-js');
+  testResourceLoads('bootstrap.js', '#bootstrap-js');
 });
