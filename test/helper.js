dd9cb802be2c8a3ee8507c6eec56a9df2b155dc9
Nicholas Skinsacos <nskins@umich.edu>
2017-11-23 12:02:11 -0500

    Fix tests: URL should not redirect to 404 page

--- test/helper.js
+++ test/helper.js
@@ -1,13 +1,16 @@
 const Nightmare = require('nightmare');
 
 // This helper ensures that the specified url loads without error.
+// It does so by checking that the 404 page did not appear.
 var pageLoads = (url, done) => {
   new Nightmare()
     .goto(url)
-    .url() // The final state of the nightmare process can be
+    .evaluate(() => {
+      return document.querySelector('#message-404');
+    })     // The final state of the nightmare process can be
     .end() // used as an argument to the following callback.
-    .then(url => {
-      expect(url).toBe(url);
+    .then((_404) => {
+      expect(_404).toBeNull();
       done(); // Call the test function's callback at the end.
     })
     .catch(done); // Catch an error and pass the callback.
