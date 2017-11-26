d9bc5786609e638a1bdc0d470a7ae773dbee5d2f
Nicholas Skinsacos <nskins@umich.edu>
2017-11-25 21:13:59 -0500

    Increase timeout window for Nightmare tests

--- test/config.js
+++ test/config.js
@@ -1,3 +1,6 @@
 const { URL } = require('url');
 
+// Some Nightmare tests require a large timeout window.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
 module.exports.HOST = new URL('http://localhost:3000');
